46d9494b167a1bf70a0329d9f4e4a702adea70a6
Crystal E <crystalegbert@westernwyoming.edu>
2025-02-13 11:31:39 -0700

    Update main.py

--- Areas-of-Rectangles/main.py
+++ Areas-of-Rectangles/main.py
@@ -1,21 +1,36 @@
-#
-# Name
-# Date
+
+# Crystal Egbert
+# 2/7/2025
 # Areas of Rectangles Programming Project
 # COSC 1010
 #
 # Local variables
+# Get the dimentions of the rectangle. 
+
 
 # Get length A
+length1 = int(input('Enter the length of the rectrangle 1: '))
 
 # Get width A
+width1 = int(input('Enter the width of rectangle 1: '))
 
 # Get length B
+length2 = int(input('Enter the length of rectangle 2: '))
 
 # Get width B
+width2 = int(input('Enter the width of the rectangle 2: '))
 
 # Calculate area A
+area1 = length1 * width1
 
 # Calculate area B
+area2 = length2 * width2
 
 # Print area comparison using if-elif-else statements
+if area1 > area2:
+    print('Rectangle 1 has the greaeter area.')
+else:
+    if area2 > area1:
+        print('Rectangle 2 has the grater area.')
+    else:
+        print('Both have same area.')
