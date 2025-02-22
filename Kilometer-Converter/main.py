288afbd065b20566092b289ec998118d7027bb95
Crystal E <crystalegbert@westernwyoming.edu>
2025-02-22 19:33:21 +0000

    I want to go someplace I need to use this!

--- Kilometer-Converter/main.py
+++ Kilometer-Converter/main.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
+# Crystal Egbert
+# 2/22/2025
 # Kilometer Converter Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+ 
+# Variables
+conversion_formula = 0.6214
+
+# Enter miles per hour to be converted
+def main ():
+    # Get the distance in kilomters.
+    kilometers = float(input("Enter a distance in kilometers: "))
+    
+    # Display distance after conversion
+    show_miles(kilometers)
+
+# Define show_miles
+def show_miles(km):
+
+    # Run calculation
+    miles = km * conversion_formula
+
+    # Display miles
+    print (km, "kilometers equals", miles, "miles")
+
+# Call to action
+main ()
