80ab0c8d1cc7f689fc77f1085f868762cebcae03
Crystal E <crystalegbert@westernwyoming.edu>
2025-02-22 20:05:06 +0000

    Getting it!

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -1,7 +1,25 @@
 #
-# Name
-# Date
+#Crystal Egbert
+# 2/22/2025
 # Feet to Inches Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program.
+# Variables
+inches = 12
+
+# Get the number of feet.
+def main():
+
+    # Get number of feet
+    feet = int(input("Enter the number of feet: "))
+
+    # Convert feet to inches
+    print(feet, "feet is" , feet_to_inches (feet), "inches.")
+
+# Convert feet_to_inches function
+def feet_to_inches(feet):
+    return feet * inches
+
+    #call the main
+main()
+
