5b26c4a3dc7acd1651ff6323909d67297b4630d1
Crystal E <crystalegbert@westernwyoming.edu>
2025-03-15 18:55:27 +0000

    well that took way longer than i expected

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,31 @@
 #
-# Name
-# Date
+# Crystal Egbert
+# 3/15/25
 # Average of Numbers Programming Project
 # COSC 1010
-#
-# Use comments liberally throughout the program. 
+
+    
+# Def main
+def average_number(holder):
+
+# Name vaiables
+    total = 0  # Total of the numbers added
+    count = 0  # Count numbers added
+
+# Get the file
+    with open('numbers.txt','r') as file:
+
+        for line in file:           # Read every line
+            number = int(line)      # Change the str into int
+            total += number         # Find total of all numbers
+            count += 1              # Find
+
+# Find the average of the numbers
+    if count > 0:
+        average = total / count     #divide to find average
+        print(f"Average:  {average}")
+    else:
+        print("No average available.")
+
+# Call the funtion
+average_number('numbers.txt')
