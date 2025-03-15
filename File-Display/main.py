1fdd5be7f54ef38e0482c878cb3db8d5fb4d5190
Crystal E <crystalegbert@westernwyoming.edu>
2025-03-15 16:31:13 +0000

    She's ready for ya!

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,16 @@
 #
-# Name
-# Date
+# Crystal Egbert
+# 3/15/25
 # File Display Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Open file
+myfile = open('numbers.txt','r')
+
+# Read and display the files contents
+for line in myfile:   # Read every line
+    number = int(line) # convert str to int
+    print(number)   # print number as int
+
+# Close the file
+myfile.close 
