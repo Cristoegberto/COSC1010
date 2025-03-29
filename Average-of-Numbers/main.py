b7ba55d563431cef3aec1f20eb0a72b64229223c
Crystal E <crystalegbert@westernwyoming.edu>
2025-03-29 00:13:45 +0000

    mic drop

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -12,8 +12,10 @@ def average_number(holder):
     total = 0  # Total of the numbers added
     count = 0  # Count numbers added
 
+try:
+
 # Get the file
-    with open('numbers.txt','r') as file:
+    with open ('numbers.txt','r') as file:
 
         for line in file:           # Read every line
             number = int(line)      # Change the str into int
@@ -26,6 +28,11 @@ def average_number(holder):
         print(f"Average:  {average}")
     else:
         print("No average available.")
+        
+except ValueError:
+    print('There was a ValueError.')
+except IOError:
+    print('There was a IOError.')
 
 # Call the funtion
 average_number('numbers.txt')
