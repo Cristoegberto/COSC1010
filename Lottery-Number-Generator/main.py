04bd61c69cb67b0664370d0e9a45d454eb5cf1cd
Crystal E <crystalegbert@westernwyoming.edu>
2025-04-03 17:27:22 +0000

    well this was interesting..Done

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,26 @@
 #
-# Name
-# Date
+# Crystal Egbert
+# 3/28/25
 # Lottery Number Generator Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# import 
+import random 
+
+# define functions
+MAX_DIGITS = 7   # max number of digits
+START = 0        # start of the random number range
+END = 9          # end of random number range
+
+# main function
+def main():
+ # create list
+
+# make loop to iterate 7 times
+
+
+# display the random numbers
+
+
+#call main
+main()
