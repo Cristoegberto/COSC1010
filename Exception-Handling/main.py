9c3878f3a6129099a9e0e2c59743b2e7de141817
Crystal E <crystalegbert@westernwyoming.edu>
2025-04-03 17:29:55 +0000

    think i got it this time

--- Exception-Handling/main.py
+++ Exception-Handling/main.py
@@ -1,7 +1,38 @@
 #
-# Name
-# Date
-# Exception Handling Programming Project
+# Crystal Egbert
+# 3/15/25
+# Average of Numbers Programming Project
 # COSC 1010
-#
-# Use comments liberally throughout the program. 
+
+    
+# Def main
+def average_number(holder):
+
+# Name vaiables
+    total = 0  # Total of the numbers added
+    count = 0  # Count numbers added
+
+try:
+
+# Get the file
+    with open ('numbers.txt','r') as file:
+
+        for line in file:           # Read every line
+            number = int(line)      # Change the str into int
+            total += number         # Find total of all numbers
+            count += 1              # Find
+
+# Find the average of the numbers
+    if count > 0:
+        average = total / count     #divide to find average
+        print(f"Average:  {average}")
+    else:
+        print("No average available.")
+        
+except ValueError:
+    print('There was a ValueError.')
+except IOError:
+    print('There was a IOError.')
+
+# Call the funtion
+average_number('numbers.txt')
--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -7,20 +7,14 @@
 # import 
 import random 
 
-# define functions
-MAX_DIGITS = 7   # max number of digits
-START = 0        # start of the random number range
-END = 9          # end of random number range
 
-# main function
-def main():
- # create list
+# define functions
+numbers = [0] * 7    #repeat this element 7 times
 
 # make loop to iterate 7 times
-
+for index in range (7):
+    #pick random number 0-9
+    numbers[index] = random.randint (0, 9)   
 
 # display the random numbers
-
-
-#call main
-main()
+print(numbers) 
