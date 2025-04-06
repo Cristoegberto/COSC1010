d2ba98c9fd87b02f50ce24dcde54ffd1f7837adb
Crystal E <crystalegbert@westernwyoming.edu>
2025-04-06 00:52:35 +0000

    modified cuz why not

--- Magic-8-Ball/main.py
+++ Magic-8-Ball/main.py
@@ -1,7 +1,34 @@
 #
-# Name
-# Date
+# Crystal Egbert
+# 4/5/25
 # Magic 8 Ball Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# import file
+import random
+
+# hold responses
+responses =[]
+
+ # begin try except
+try:                 
+    with open('8_ball_responses.txt','r') as file:
+      responses = file.readlines()  #read lines and create a file to a list
+
+# prompt user for a question
+    while True:
+        question = input('Ask my Mystical, Magical 8 Ball youre undying questions: \nType "Done" if you\'ve satified your queries.')
+
+# quit or break loop
+        if question == 'Done':
+            print('Adios!')
+            break
+
+# display randomly selected response from the list
+    print(random.choice(responses).strip())
+
+#check for errors
+except FileNotFoundError:
+    print('The file was not found.')
+except IOError:
+    print('There was a IOError.') 
