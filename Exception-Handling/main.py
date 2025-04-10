2a778e0d9c49d40ca361040ea5a4e9556c8c496e
Crystal E <crystalegbert@westernwyoming.edu>
2025-04-10 01:41:01 +0000

    modified still not fixed

--- Exception-Handling/main.py
+++ Exception-Handling/main.py
@@ -4,35 +4,33 @@
 # Average of Numbers Programming Project
 # COSC 1010
 
-    
+
 # Def main
 def average_number(holder):
 
-# Name vaiables
-    total = 0  # Total of the numbers added
-    count = 0  # Count numbers added
-
-try:
-
+    try:
+         # Name vaiables
+        total = 0  # Total of the numbers added
+        count = 0  # Count numbers added
+        
 # Get the file
-    with open ('numbers.txt','r') as file:
+        with open ('numbers.txt','r') as file:
 
-        for line in file:           # Read every line
-            number = int(line)      # Change the str into int
-            total += number         # Find total of all numbers
-            count += 1              # Find
+            for line in file:           # Read every line
+                number = int(line)      # Change the str into int        
+                count += 1              # Find total of all numbers
 
 # Find the average of the numbers
-    if count > 0:
-        average = total / count     #divide to find average
-        print(f"Average:  {average}")
-    else:
-        print("No average available.")
+        if count > 0:
+            average = total / count     #divide to find average
+            print(f"Average:  {average}")
+        else:
+            print("No average available.")
         
-except ValueError:
-    print('There was a ValueError.')
-except IOError:
-    print('There was a IOError.')
+    except ValueError:
+        print('There was a ValueError.')
+    except IOError:
+        print('There was a IOError.')
 
 # Call the funtion
 average_number('numbers.txt')
